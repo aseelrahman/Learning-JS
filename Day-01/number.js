9ffe5b4f4f08c92a51978b8763f655a7ba6b8d97
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2025-03-03 14:13:59 +0000

    number

---
+++ Day-01/number.js
@@ -0,0 +1,13 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+var applePrice = "25";
+
+console.log(typeof orangePrice);
+console.log(typeof chocolate)
+console.log(typeof parseInt(applePrice));
+
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+console.log(total.toFixed(2));
