327b4ab734cf32a7d3050bc227c367a9ca651ccc
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2025-03-06 16:06:40 +0000

    while loop

---
+++ Day-03/while.js
@@ -0,0 +1,13 @@
+const numbers = 7;
+if(numbers < 10){
+    console.log('choto ekta number paisi');
+}
+console.log('something new is cooking');
+
+// ------------------------- WHILE--------LOOP____________________
+
+let num = 0;
+while(num < 5){
+    console.log('loooooping');
+    num += 1;
+}
