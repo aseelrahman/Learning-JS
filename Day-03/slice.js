1098c7fc005a5418205fd17975e510727027c550
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2025-03-06 20:27:10 +0000

    slice and join

---
+++ Day-03/slice.js
@@ -0,0 +1,12 @@
+const address = 'andorkilla';
+const part = address.slice(2, 5);
+console.log(part);
+
+const sentence = 'I am a good and hardworking person.';
+console.log(sentence.split(" "));
+
+
+const words = [ 'I', 'am', 'a', 'good', 'and', 'hardworking', 'person.' ];
+
+console.log(words.join(" | "));
+
