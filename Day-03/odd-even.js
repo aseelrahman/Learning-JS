50411d365d6e38437b4b04e150f9c01c548798db
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2025-03-06 19:32:01 +0000

    odd-even

---
+++ Day-03/odd-even.js
@@ -0,0 +1,15 @@
+for(let i = 61; i <=100; i++){
+    if (i%2 !== 0) {
+        console.log(i);
+        
+    }
+}
+
+console.log("");
+
+for (let e = 78; e <= 98; e++){
+   if (e%2 == 0) {
+    console.log(e);
+    
+   }
+}
