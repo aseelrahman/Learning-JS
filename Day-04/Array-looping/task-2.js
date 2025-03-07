28258637733ec5b257f6dd28996742359c9a8eca
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2025-03-07 21:28:39 +0000

    Array task-2

---
+++ Day-04/Array-looping/task-2.js
@@ -0,0 +1,10 @@
+const numbers = [12, 98, 5, 41, 23, 78, 46];
+let evenNumbers = [];
+
+for(num of numbers){
+    if(num%2 == 0){
+        evenNumbers.push(num)
+    }
+}
+
+console.log(evenNumbers);
