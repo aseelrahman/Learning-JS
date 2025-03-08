08a0715390a89739adaa6cf843d4ebfc6e1b8590
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2025-03-08 18:33:24 +0000

    Function task-1

---
+++ Day-05/Function/task-01.js
@@ -0,0 +1,13 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiply_nums(num1, num2, num3, num4) {
+    let result = num1*num2*num3*num4;
+
+    return result;
+}
+
+let result = multiply_nums(2, 3, 5, 4)
+
+console.log(result);
+
+
