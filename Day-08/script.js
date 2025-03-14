f52c94bcbabedf04a3c4d9ba352050dea4d484a8
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2025-03-14 15:39:30 +0000

    Day-08: files copied from day07

---
+++ Day-08/script.js
@@ -0,0 +1,16 @@
+const fruits = document.getElementsByClassName("fruits");
+for(const fruit of fruits){
+    console.log(fruit.innerText);
+}
+
+const fruitTitle = document.getElementById("fruit-title");
+fruitTitle.innerText = 'Inshallah I\'ll try these fruits.'
+
+// querySelectorAll
+
+const someLi = document.querySelectorAll("#fruits-container li")
+console.log(someLi);
+for(const li of someLi){
+    console.log(li.innerText);
+    
+}
