ffe79f4b35d1938217adec6704a306c8c867a455
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2025-03-14 16:32:32 +0000

    learned how to add and remove class via js.

--- Day-08/script.js
+++ Day-08/script.js
@@ -1,20 +1,35 @@
-const fruits = document.getElementsByClassName("fruits");
-for(const fruit of fruits){
-    console.log(fruit.innerText);
-}
+// const fruits = document.getElementsByClassName("fruits");
+// for(const fruit of fruits){
+//     console.log(fruit.innerText);
+// }
 
-const fruitTitle = document.getElementById("fruit-title");
-fruitTitle.innerText = 'Inshallah I\'ll try these fruits.'
+// const fruitTitle = document.getElementById("fruit-title");
+// fruitTitle.innerText = 'Inshallah I\'ll try these fruits.'
 
-// querySelectorAll
+// // querySelectorAll
 
-const someLi = document.querySelectorAll("#fruits-container li")
-console.log(someLi);
-for(const li of someLi){
-    console.log(li.innerText);
+// const someLi = document.querySelectorAll("#fruits-container li")
+// console.log(someLi);
+// for(const li of someLi){
+//     console.log(li.innerText);
     
+// }
+
+
+// fruitTitle.getAttribute('id')
+// fruitTitle.setAttribute('title', 'giving a title')
+
+
+const sections = document.querySelectorAll('section');
+
+for(const section of sections){
+    section.style.border = '2px solid steelblue';
+    section.style.marginBottom = "5px";
+    section.style.borderRadius = '10px';
+    section.style.padding = "10px";
+    section.style.backgroundColor = "lightgray";
 }
+const fruitContainer = document.getElementById('fruits-to-try');
+fruitContainer.classList.add('text-center');
+fruitContainer.classList.remove('large-text')
 
-
-fruitTitle.getAttribute('id')
-fruitTitle.setAttribute('title', 'giving a title')
