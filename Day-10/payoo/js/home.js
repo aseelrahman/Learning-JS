d7e1b51de3b47ceea798b4681b0a80a23a8cc430
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2025-03-16 18:59:28 +0000

    Handled add money event handler on the button click

--- Day-10/payoo/js/home.js
+++ Day-10/payoo/js/home.js
@@ -1,1 +1,13 @@
-console.log('Home Added!');
+// Add money to the account
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault(); // prevent page reload after form submit
+
+    // step-2: get money to be added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+    
+    // step-3: get the pin number provided
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput);
+    
+})
