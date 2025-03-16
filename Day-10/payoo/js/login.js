217e98cacc42308d7544e6b1e9dab9528a20671f
aseelrahman <132919956+aseelrahman@users.noreply.github.com>
2025-03-16 17:32:24 +0000

    Handled login and redirected to the home page

--- Day-10/payoo/js/login.js
+++ Day-10/payoo/js/login.js
@@ -1,8 +1,19 @@
 document.getElementById('btn-login').addEventListener('click', function (event) {
     event.preventDefault(); // prevent form default behavior
-    console.log('login button clicked');
-    const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
 
+    // Get Phone and PIN number.
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+
+    console.log(phoneNumber, pinNumber);
+
+    // This is temporary. You should not do like this!
+    if (phoneNumber === '5' && pinNumber === '1234') {
+        console.log('You are logged in');
+        window.location.href = 'home.html';
+    }
+    else {
+        alert('Wrong phone number or pin!')
+    }
 })
 
